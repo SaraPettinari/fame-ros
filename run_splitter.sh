f70030a980886302f318c8ef16ad9e4b485ce468
SaraPettinari <sara.pettinari@unicam.it>
2024-01-09 10:28:08 +0100

    [refactoring] Splitter node functionalities

---
+++ run_splitter.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+clear
+colcon build
+ros2 launch fame_engine splitter.launch.py
--- src/splitter.js
+++ src/splitter.js
@@ -1,13 +1,7 @@
 'use strict';
 const rclnodejs = require('rclnodejs');
-const fs = require('fs');
 const xml2json = require('xml2json')
 
-
-var source = '';
-var process_path = '/home/ubuntu/mbros/fame_engine/process/';
-var process_dict = {};
-var process_robot = {};
 
 const definitions = 'bpmn:definitions';
 const collaboration = 'bpmn:collaboration';
@@ -15,64 +9,83 @@ const participant = 'bpmn:participant';
 const processes = 'bpmn:process';
 const signals = 'bpmn:signal';
 
-fs.readFile(process_path + 'simple_scenario.bpmn', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    source = data;
-})
 
+/**
+ * Extraction of the bpmn process models from the received data
+ * @param {*} node the ROS node
+ */
+function splitter(node) {
 
-function splitter() {
-    var xml = source;
-    var jsonstring = xml2json.toJson(xml)
-    var jsonbpmn = JSON.parse(jsonstring)
+    // creation of the subscription over the /collaboration_diagram topic
+    node.createSubscription('std_msgs/msg/String', '/collaboration_diagram', (msg) => {
+        var process_dict = {};
+        var process_robot = {};
 
-    var process_definition = jsonbpmn[definitions]
-    var process_participants = process_definition[collaboration][participant]
+        node.getLogger().info(`Received collaboration diagram`);
+        var xml = msg.data;
+        // conversion to an object
+        var jsonstring = xml2json.toJson(xml)
+        var jsonbpmn = JSON.parse(jsonstring)
 
-    for (let i = 0; i < process_participants.length; i++) {
-        var process = process_participants[i]
-        var p = structuredClone(process)
-        var d = structuredClone(process_definition)
-        process_dict[process.processRef] = {};
-        process_dict[process.processRef][definitions] = d;
-        process_dict[process.processRef][definitions][collaboration][participant] = p;
-    }
+        var process_definition = jsonbpmn[definitions]
+        var process_participants = process_definition[collaboration][participant]
 
-    Object.keys(process_dict).forEach(element => {
-        process_dict[element][definitions][signals] = process_definition[signals]
-        var extract_process = process_dict[element][definitions][processes]
-        //console.log(extract_process)
-        var curr_process = extract_process.find(p => p.id == element);
-        process_dict[element][definitions][processes] = curr_process;
-        curr_process['isExecutable'] = true
-        var x = process_dict[element];
-        var conv_xml = xml2json.toXml(x)
+        // for each participant, split the collaboration in the collaboration
+        for (let i = 0; i < process_participants.length; i++) {
+            var process = process_participants[i]
+            var tempProcess = structuredClone(process)
+            var tempDefinition = structuredClone(process_definition)
+            process_dict[process.processRef] = {};
+            process_dict[process.processRef][definitions] = tempDefinition;
+            process_dict[process.processRef][definitions][collaboration][participant] = tempProcess;
+        }
 
-        var robot = element.replace('Process_', '').toUpperCase();
-        process_robot[robot] = conv_xml;
-    })
+        // creation of a dictionary {robot_id: bpmn_process, ...}
+        Object.keys(process_dict).forEach(element => {
+            process_dict[element][definitions][signals] = process_definition[signals]
+            var extract_process = process_dict[element][definitions][processes]
+            //console.log(extract_process)
+            var curr_process = extract_process.find(p => p.id == element);
+            process_dict[element][definitions][processes] = curr_process;
+            curr_process['isExecutable'] = true
+            var x = process_dict[element];
+            var conv_xml = xml2json.toXml(x)
 
+            var robot = element.replace('Process_', '').toUpperCase();
+            process_robot[robot] = conv_xml;
+        })
+
+        // publication of the process models
+        if (process_robot) {
+            Object.keys(process_robot).forEach(robot => {
+                var bpmn = process_robot[robot]
+                publish_msg(node, robot, bpmn)
+            });
+        }
+    });
 }
 
+/**
+ * Publishes bpmn process models over the /bpmn_process ros topic
+ * @param {*} node the ROS node
+ * @param {*} robot robot identifier
+ * @param {*} bpmn robot process model
+ */
 function publish_msg(node, robot, bpmn) {
     var publisher = node.createPublisher('std_msgs/msg/String', robot + '/bpmn_process');
 
     setTimeout(function () {
         publisher.publish(bpmn);
-        console.log(`Publishing process of ${robot}`);
+        console.log(`Publishing process of ${robot}...`);
     }, 2000);
 }
 
+/**
+ * Splitter node initialization
+ */
 rclnodejs.init().then(() => {
-    splitter();
     const node = new rclnodejs.Node('splitter_node');
-    Object.keys(process_robot).forEach(robot => {
-        var bpmn = process_robot[robot]
-        publish_msg(node, robot, bpmn)
-    });
+    splitter(node);
 
     node.spin();
 });
